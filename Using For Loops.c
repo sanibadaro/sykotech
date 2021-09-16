03c322eacb348a595803acb9228aa77001bf29c2
sanibadaro <90673904+sanibadaro@users.noreply.github.com>
2021-09-15 23:47:01 -0700

    Add files via upload

---
+++ Using For Loops.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+    //Variable Declaration
+	   int a=10;	
+
+    //Using For Loops
+     for(a=10;  a<=500; a++)
+    {
+    	printf("%d \n", a);
+	}
+	return 0;
+}
